edde0218e27aa66d83442de0a1f577fa04efbb7a
Sasawat Chanate <sasawatc@gmail.com>
2018-11-01 11:55:04 -0700

    Fixed drop columns in init_data.py
    * fixed issues in init_data v0.5.0 where 'adult_literacy_pct', 'homicides_per_100k', 'tax_revenue_pct_gdp' not dropped

--- init_data.py
+++ init_data.py
@@ -7,7 +7,7 @@ For initializing datasets
 
 from pandas import DataFrame
 
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 __author__ = ('Joshua Thang, Kaiyi Zou, Khuyen Yu, '
               'Kristian Nielsen, Sasawat Chanate, Ying Li')
 
@@ -18,6 +18,8 @@ import pandas as pd
 
 from handy_func import *
 
+##############################################################################################
+# declare directory paths
 base_folder = Path('data/base')
 processed_folder = Path('data/processed')
 output_folder = Path('output')
@@ -25,10 +27,16 @@ output_folder = Path('output')
 og_file = base_folder / 'world_data_hult_regions.xlsx'
 data_set: DataFrame = pd.read_excel(og_file)
 
+##############################################################################################
 # fix typos from base file
 data_set.loc[data_set.Hult_Team_Regions == "Central Aftica 1", 'Hult_Team_Regions'] = 'Central Africa 1'
 data_set.rename(columns={'CO2_emissions_per_capita)': 'CO2_emissions_per_capita'}, inplace=True)
 
+##############################################################################################
+# drop bad columns
+data_set = data_set.drop(columns=['adult_literacy_pct', 'homicides_per_100k', 'tax_revenue_pct_gdp']).copy()
+
+##############################################################################################
 # flag columns with missing value
 """ Create columns that are 0s if a value was not missing and 1 if
 a value is missing. """
@@ -51,32 +59,35 @@ data_set = pd.merge(extracted_df, data_set,
 data_set = data_set.drop(columns=['Country Code'])
 
 ##############################################################################################
-# drop bad columns
-data_set.drop(columns=['adult_literacy_pct', 'homicides_per_100k', 'tax_revenue_pct_gdp'])
-
-##############################################################################################
-# fill in the remaining NAs with mean/median based on skewness
+# fill in the remaining NAs in numeric columns with mean/median based on skewness
 for col in data_set.select_dtypes(include=['float64', 'int']).columns:
     data_set[col] = replace_na_mean_median(col_series=data_set[col])
 
 ##############################################################################################
 # reorder gni_index to be right before gdp_usd
-data_set = data_set[['country_index', 'Hult_Team_Regions', 'country_name',
-                     'country_code', 'income_group', 'access_to_electricity_pop',
-                     'access_to_electricity_rural', 'access_to_electricity_urban',
-                     'CO2_emissions_per_capita', 'compulsory_edu_yrs', 'pct_female_employment',
-                     'pct_male_employment', 'pct_agriculture_employment',
-                     'pct_industry_employment', 'pct_services_employment',
-                     'exports_pct_gdp', 'fdi_pct_gdp', 'gni_index', 'gdp_usd', 'gdp_growth_pct',
-                     'incidence_hiv', 'internet_usage_pct', 'homicides_per_100k',
-                     'adult_literacy_pct', 'child_mortality_per_1k', 'avg_air_pollution',
-                     'women_in_parliament', 'tax_revenue_pct_gdp', 'unemployment_pct',
-                     'urban_population_pct', 'urban_population_growth_pct', 'm_compulsory_edu_yrs',
-                     'm_incidence_hiv', 'm_homicides_per_100k', 'm_adult_literacy_pct',
-                     'm_tax_revenue_pct_gdp']]
+data_set = data_set[['country_index', 'Hult_Team_Regions', 'country_name', 'country_code',
+                     'income_group', 'access_to_electricity_pop', 'access_to_electricity_rural',
+                     'access_to_electricity_urban', 'CO2_emissions_per_capita', 'compulsory_edu_yrs',
+                     'pct_female_employment', 'pct_male_employment', 'pct_agriculture_employment', 'gni_index',
+                     'pct_industry_employment', 'pct_services_employment', 'exports_pct_gdp', 'fdi_pct_gdp',
+                     'gdp_usd', 'gdp_growth_pct', 'incidence_hiv', 'internet_usage_pct',
+                     'child_mortality_per_1k', 'avg_air_pollution', 'women_in_parliament', 'unemployment_pct',
+                     'urban_population_pct', 'urban_population_growth_pct', 'm_income_group',
+                     'm_access_to_electricity_pop', 'm_access_to_electricity_rural',
+                     'm_access_to_electricity_urban', 'm_CO2_emissions_per_capita', 'm_compulsory_edu_yrs',
+                     'm_pct_female_employment', 'm_pct_male_employment', 'm_pct_agriculture_employment',
+                     'm_pct_industry_employment', 'm_pct_services_employment', 'm_exports_pct_gdp',
+                     'm_fdi_pct_gdp', 'm_gdp_usd', 'm_gdp_growth_pct', 'm_incidence_hiv', 'm_internet_usage_pct',
+                     'm_homicides_per_100k', 'm_adult_literacy_pct', 'm_child_mortality_per_1k',
+                     'm_avg_air_pollution', 'm_women_in_parliament', 'm_tax_revenue_pct_gdp', 'm_unemployment_pct',
+                     'm_urban_population_pct', 'm_urban_population_growth_pct']]
+
+##############################################################################################
 # export
 data_set.to_excel(output_folder / 'clean_data.xlsx')
 
 # subset central_africa1 and export
 central_africa1_df: DataFrame = data_set[data_set['Hult_Team_Regions'] == "Central Africa 1"].copy()
 central_africa1_df.to_excel(output_folder / "clean_data_central_africa.xlsx")
+
+##############################################################################################
